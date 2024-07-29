280c223b48c3d82566af11616703113dd864b4e8
Kirill <42931612+Kiriuha-lucky@users.noreply.github.com>
2024-07-29 04:00:55 +0300

    f

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,68 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+export default ({ env }) => {
+	return [
+		'strapi::logger',
+		'strapi::errors',
+		{
+			name: 'strapi::security',
+			config: {
+				contentSecurityPolicy: {
+					useDefaults: true,
+					directives: {
+						'connect-src': ["'self'", 'https:'],
+						'script-src': [
+							"'self'",
+							'editor.unlayer.com',
+							'https://editor.unlayer.com',
+						],
+						'frame-src': [
+							"'self'",
+							'editor.unlayer.com',
+							'https://editor.unlayer.com',
+						],
+						'img-src': [
+							"'self'",
+							'data:',
+							'blob:',
+							'dl.airtable.com',
+							'337acbc3-097fc636-c32e-4acd-ba2c-f619436e64d7.s3.timeweb.cloud',
+							'cdn.jsdelivr.net',
+							'strapi.io',
+							's3.amazonaws.com',
+						],
+						'media-src': [
+							"'self'",
+							'data:',
+							'blob:',
+							'dl.airtable.com',
+							'337acbc3-097fc636-c32e-4acd-ba2c-f619436e64d7.s3.timeweb.cloud',
+						],
+						upgradeInsecureRequests: null,
+					},
+				},
+			},
+		},
+		{
+			name: 'strapi::cors',
+			config: {
+				enabled: true,
+				origin: env.array('ORIGINS'),
+			},
+		},
+		'strapi::poweredBy',
+		'strapi::query',
+		{
+			name: 'strapi::body',
+			config: {
+				formLimit: '10mb', // modify form body
+				jsonLimit: '10mb', // modify JSON body
+				textLimit: '10mb', // modify text body
+				formidable: {
+					maxFileSize: 20 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+				},
+			},
+		},
+		'strapi::session',
+		'strapi::favicon',
+		'strapi::public',
+	];
+};
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,23 @@
-export default () => ({});
+export default ({ env }) => ({
+	upload: {
+		config: {
+			provider: 'aws-s3',
+			providerOptions: {
+				accessKeyId: env('AWS_ACCESS_KEY_ID'),
+				secretAccessKey: env('AWS_ACCESS_SECRET'),
+				region: env('AWS_REGION'),
+				endpoint: env('AWS_BUCKET_URL'),
+				params: {
+					Bucket: env('AWS_BUCKET'),
+				},
+				sizeLimit: 20 * 1024 * 1024,
+			},
+			actionOptions: {
+				get: {},
+				upload: {},
+				uploadStream: {},
+				delete: {},
+			},
+		},
+	},
+});
